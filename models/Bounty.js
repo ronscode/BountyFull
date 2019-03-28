055651441492d2111c23ff51aed64780d3a51d64
Ron Cagle <ron@atlantashirt.com>
2019-03-28 14:21:54 -0400

    all files

--- models/Bounty.js
+++ models/Bounty.js
@@ -1,54 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // User Schema
 const BountySchema = new mongoose.Schema({
-    poster: {
-        type: String,
-        required: true
-    },
-    claimer: {
-        type: String,
-    },
-    bountyAmount:{
-        type: Number,
-        required: true
-    },
-    isStarted: {
-        type: Boolean,
-        required: true
-    },
-    isCleaned: {
-        type: Boolean,
-        required: true
-    },
-    isVerified: {
-        type: Boolean,
-        required: true
-    },
-    isPaid: {
-        type: Boolean,
-        required: true
-    },
-    isComplete: {
-        type: Boolean,
-        required: true
-    },
-    location: {
-        type: mongoose.Mixed,
-        required: true
-    },
-    pictures: {
-        type: mongoose.Mixed,
-        required: true
-    },
-    timeStarted: {
-        type: String
-    },
-    timeEnded: {
-        type: String
-    },
+  poster: {
+    type: String,
+    required: true
+  },
+  bountyTitle: {
+    type: String
+  },
+  claimer: {
+    type: String
+  },
+  bountyAmount: {
+    type: Number
+  },
+  isStarted: {
+    type: Boolean,
+    required: true
+  },
+  isCleaned: {
+    type: Boolean,
+    required: true
+  },
+  isVerified: {
+    type: Boolean,
+    required: true
+  },
+  isPaid: {
+    type: Boolean,
+    required: true
+  },
+  isComplete: {
+    type: Boolean,
+    required: true
+  },
+  location: {
+    type: mongoose.Mixed,
+    required: true
+  },
+  pictures: {
+    type: mongoose.Mixed,
+    required: true
+  },
+  timeStarted: {
+    type: String
+  },
+  timeEnded: {
+    type: String
+  }
 });
 
-const Bounty = mongoose.model('Bounty', BountySchema);
+const Bounty = mongoose.model("Bounty", BountySchema);
 
 module.exports = Bounty;
